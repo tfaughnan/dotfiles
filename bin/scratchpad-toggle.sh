117320a7bed993be77e2db187026f385dd6916b6
Thomas Faughnan <tom@tjf.sh>
2021-12-12 05:28:08 -0500

    clean up minmax stuff, nop Menu key (was messing w/ vim)

--- bin/scratchpad-toggle.sh
+++ bin/scratchpad-toggle.sh
@@ -2,7 +2,7 @@
 
 # if we accidentally killed the scratchpad, resurrect it
 if ! pgrep -f kitty_scratchpad > /dev/null; then
-    kitty --name kitty_scratchpad ~/bin/scratchpad-launch.sh &
+    kitty --name kitty_scratchpad -c ~/.config/kitty/scratchpad.conf ~/bin/scratchpad-launch.sh &
 fi
 
 id=$(cat /tmp/scratchpad_id)
