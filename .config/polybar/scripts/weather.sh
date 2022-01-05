de06293bdf6aff090b4de1bd2e7938c67996aa5e
Thomas Faughnan <tom@tjf.sh>
2022-01-05 08:50:00 -0500

    big things

--- .config/polybar/scripts/weather.sh
+++ .config/polybar/scripts/weather.sh
@@ -9,17 +9,18 @@ data=$(curl -sf "${ONECALL}?lat=${LAT}&lon=${LON}&units=${UNITS}&appid=${OW_KEY}
 temp=$(echo "$data" | jq -r .current.temp)
 weather=$(echo "$data" | jq -r .current.weather[0].icon)
 
+# see https://openweathermap.org/weather-conditions for condition codes
 case "$weather" in
-    01d) icon= ;;
-    01n) icon= ;;
-    02d|04d) icon= ;;
-    02n|04n) icon= ;;
-    03d|03n) icon= ;;
-    09d|09n|10d|10n) icon= ;;
-    11d|11n) icon= ;;
-    13d|13n) icon= ;;
-    50d|50n) icon= ;;
-    *) icon= ;;
+    01d) icon= ;;                  # clear sky (day)
+    01n) icon= ;;                  # clear sky (night)
+    02d|04d) icon= ;;              # few clouds, broken clouds (day)
+    02n|04n) icon= ;;              # few clouds, broken clouds (night)
+    03d|03n) icon= ;;              # scattered clouds (day or night)
+    09d|09n|10d|10n) icon= ;;      # shower rain, rain (day or night)
+    11d|11n) icon= ;;              # thunderstorm (day or night)
+    13d|13n) icon= ;;              # snow (day or night)
+    50d|50n) icon= ;;              # mist (day or night)
+    *) icon= ;;                    # other
 esac
 
 echo "$icon $temp °F"
